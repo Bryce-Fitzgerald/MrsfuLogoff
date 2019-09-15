953514f244265b2804e9cb91cf21cb68b407dcc3
Bryce-Fitzgerald <55312792+Bryce-Fitzgerald@users.noreply.github.com>
2019-09-15 22:15:26 +0800

    完成框架搭建
    目前尚为单机版，后台处理留待后期更新

--- MrsfuLogoff/app/src/main/java/com/example/mrsfulogoff/UserActivity.java
+++ MrsfuLogoff/app/src/main/java/com/example/mrsfulogoff/UserActivity.java
@@ -1,41 +1,40 @@
 package com.example.mrsfulogoff;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.example.mrsfulogoff.MainActivity;
-import com.example.mrsfulogoff.R;
-import com.example.mrsfulogoff.SelfTestActivity;
-import com.example.mrsfulogoff.TitleBuilder;
 
 public class UserActivity extends AppCompatActivity {
+    public static Context mContext;
 
-
-
-private void initView() {
-        new TitleBuilder(this).setTitleText("用户信息").setButtonClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-        }
-        }).build();
-        }
-
-public static final String TAG="MainActivity";
-@ Override
-protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
-        Button startSelfTestActivity = (Button) findViewById(R.id.start_selftest_activity);
-
-        startSelfTestActivity.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-        Intent intent = new Intent(UserActivity.this, SelfTestActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_user);
+        mContext=this.getBaseContext();
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null) {
+            actionBar.hide();
         }
-        });
-        }}
+        initTitleView();
+        initBottomView();
+    }
+
+    private void initTitleView() {
+        new TitleBuilder(this).setTitleText("我").setButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(UserActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        }).build();
+    }
+    private void initBottomView() {
+        new BottomBuilder(this).setHpButtonClickListener(this).setNsButtonClickListener(this).setClButtonClickListener(this).setTrButtonClickListener(this).build();
+    }
+}
+
+
